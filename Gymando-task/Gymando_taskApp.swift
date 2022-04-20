0c97597f96de96eb01ce024fa7b097b1870512a2
Ahmed Shahid <ahmed.shahidpk@gmail.com>
2022-04-20 21:51:10 +0200

    Project created

---
+++ Gymando-task/Gymando_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gymando_taskApp.swift
+//  Gymando-task
+//
+//  Created by Ahmed Shahid on 20.04.22.
+//
+
+import SwiftUI
+
+@main
+struct Gymando_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
