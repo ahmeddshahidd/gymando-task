81609a6b688b7b35c62781ef738e4fe44acbb6ff
Ahmed Shahid <ahmed.shahidpk@gmail.com>
2022-04-20 22:06:01 +0200

    appdelegate and scene delegate setup done

---
+++ Gymando-task/App Setup/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  Gymando-task
+//
+//  Created by Ahmed Shahid on 20.04.22.
+//
+
+import Foundation
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
